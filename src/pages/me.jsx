7fca6cd82a65aba743f2d1e3067ce9e41b6fe173
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-05-20 18:22:54 +0700

    improve: add protected route

--- src/pages/me.jsx
+++ src/pages/me.jsx
@@ -5,9 +5,15 @@ import { Authenticated } from "@/modules"
 
 const { UserModule } = lazily(() => import("@/modules"))
 
+const Loading = () => (
+  <section className="flex items-center h-screen justify-center">
+    <LoadingSpinner/>
+  </section>
+)
+
 export const Me = () => {
   return (
-    <Suspense fallback={<LoadingSpinner/>}>
+    <Suspense fallback={<Loading/>}>
       <Authenticated>
         <UserModule/>
       </Authenticated>
