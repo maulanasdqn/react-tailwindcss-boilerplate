ba86db4661724f2f273358735093eb4d125108bf
Maulana Sodiqin <53475078+maulanasdqn@users.noreply.github.com>
2023-06-02 11:27:31 +0700

    Update module.jsx

--- src/modules/user/module.jsx
+++ src/modules/user/module.jsx
@@ -6,7 +6,7 @@ export const UserModule = () => {
   const { mutate } = useLogout()
   return (
     <section className="flex flex-col items-center justify-center w-full h-screen">
-      <h1>Authenticated as {data?.data?.email}</h1>
+      <h1>Authenticated as {data?.user?.name}</h1>
       <span onClick={mutate} className="text-red-500 cursor-pointer my-4 font-medium">Logout</span>
     </section>
   )
