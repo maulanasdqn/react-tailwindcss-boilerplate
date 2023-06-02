07b3b6b32bf43ef4aeab05dcd4c9f5001ecba26d
Maulana Sodiqin <53475078+maulanasdqn@users.noreply.github.com>
2023-06-02 10:26:31 +0700

    Update module.jsx

--- src/modules/user/module.jsx
+++ src/modules/user/module.jsx
@@ -1,12 +1,12 @@
 import TokenService from "@/services/token"
-import { useLogout } from "./hooks"
+import { useLogout, useUser } from "./hooks"
 
 export const UserModule = () => {
-  const userData = JSON.parse(TokenService.getUserData())
+  const { data } = useUser()
   const { mutate } = useLogout()
   return (
     <section className="flex flex-col items-center justify-center w-full h-screen">
-      <h1>Authenticated as {userData.email}</h1>
+      <h1>Authenticated as {data?.data?.email}</h1>
       <span onClick={mutate} className="text-red-500 cursor-pointer my-4 font-medium">Logout</span>
     </section>
   )
